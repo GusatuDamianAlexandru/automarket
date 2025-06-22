88be01a25cb819ab2f86ba765c9a3f505107a5e2
AlexDG578 <alexdg578@gmail.com>
2025-06-22 14:58:22 +0300

    modificare pentru support android chrome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'core-js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'; // Necesar pentru async/await
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
